9bda6bbe9c7a3c394c478b1631f175ba0c6f2861
TJohnW <tristan.whitcher@gmail.com>
2014-01-09 00:30:30 -0600

    Added copyright

--- Vectron.cpp
+++ Vectron.cpp
@@ -1,3 +1,27 @@
+/*
+**************************************************************************
+Vectron - map editor for Armagetron Advanced.
+Copyright (C) 2014  Tristan Whitcher    (tristan.whitcher@gmail.com)
+                    David Dubois        (ddubois@jotunstudios.com)
+**************************************************************************
+
+This file is part of Vectron.
+
+Vectron is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Vectron is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Vectron.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 #include <GLFW/glfw3.h>
 #include <cstdlib>
 #include <cstdio>
@@ -37,6 +61,7 @@ int main(void) {
     glfwSetKeyCallback(window, key_callback);
 
     while (!glfwWindowShouldClose(window)) {
+
 
         float ratio;
         int width, height;
