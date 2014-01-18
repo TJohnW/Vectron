53f2c3afd945949cc2c21828106829a5cea3d9dc
TJohnW <tristan.whitcher@gmail.com>
2014-01-18 05:19:17 -0600

    Walls

--- vectron.js
+++ vectron.js
@@ -13,18 +13,48 @@ window.onload = function() {
     $("#canvas_container").mousemove(function(event) {
         event.pageX -= 60;
         vectron.cursor.render(event.pageX, event.pageY, vectron.spacing);
+        if(vectron.map.active && vectron.map.currentObj instanceof Wall) {
+            vectron.map.currentObj.guide();
+        } else if (vectron.map.active && vectron.map.currentObj instanceof Zone) {
+
+        }
     });
 
     $("#canvas_container").on("click", function(e) {
 
             e.preventDefault();
+            if(vectron.map.active) {
+                vectron.map.currentObj.progress();
+            } else {
+                vectron.map.objects.push(new Wall(vectron));
+            }
+            /*
             vectron.map.objects.push(
                 new Zone(vectron,
                         vectron.getMapX(vectron.cursor.snappedMouseX),
                         vectron.getMapY(vectron.cursor.snappedMouseY),
                         1, "death")
             );
-    });  
+            */
+    }); 
+
+    $("#canvas_container").on("dblclick", function(e) {
+
+            e.preventDefault();
+            if(vectron.map.active && vectron.map.currentObj instanceof Wall) {
+                vectron.map.currentObj.finish();
+            } else {
+                //vectron.map.objects.push(new Wall(vectron));
+            }
+            /*
+            vectron.map.objects.push(
+                new Zone(vectron,
+                        vectron.getMapX(vectron.cursor.snappedMouseX),
+                        vectron.getMapY(vectron.cursor.snappedMouseY),
+                        1, "death")
+            );
+            */
+    });
 }
 
 
@@ -145,8 +175,12 @@ function Aamap(vectron) {
 
     this._xml = '';
     this.objects = [new Zone(vectron, 0, 0, 1, "win")];
+    this.currentObj = 0;
+
     this.paper = vectron.paper;
     this.canvas = vectron;
+    this.active = false; // is a tool active?
+
 }  
 
 Aamap.prototype = {
@@ -187,6 +221,129 @@ Zone.prototype = {
     render:function() {
         this.ob = this.paper.circle(this.canvas.getRealX(this._x),
             this.canvas.getRealY(this._y),
-            this._radius*this.canvas.spacing).attr("stroke","#FF0000");
+            this._radius*this.canvas.spacing).attr(
+                {"stroke": "#FF0000", "fill": "#FFF", "fill-opacity": "0"}
+            );
+    }
+}
+
+function WallPoint(x, y) {
+    this._x = x;
+    this._y = y;
+}
+
+function Wall(vectron) {
+
+    this._xml = '';
+    this._points = [];
+
+    this.canvas = vectron;
+    this.paper = vectron.paper;
+
+    this.pathArr = [];
+
+    this.guideOb = this.paper.path();
+    this.ob = this.paper.path();
+
+    this.start();
+    this.render();
+
+    var elem = document.getElementById('debug_stream');
+    elem.scrollTop = elem.scrollHeight;
+
+}  
+
+Wall.prototype = {
+
+    constructor: Wall,
+
+    start:function() {
+        this.canvas.map.active = true;
+        this.canvas.map.currentObj = this;
+        this._points.push(
+            new WallPoint(
+                this.canvas.getMapX(this.canvas.cursor.snappedMouseX),
+                this.canvas.getMapY(this.canvas.cursor.snappedMouseY))
+        );
+        this.render();
+    },
+
+    finish:function() {
+        this.progress();
+        if(this._points.length < 2) {
+
+            this.ob.remove();
+            this.guideOb.remove();
+        }
+        var last = this._points[this._points.length-1];
+        var secLast = this._points[this._points.length-2];
+        if(last._x == secLast._x && last._y == secLast._y) {
+            this._points.pop();
+            $('#debug_stream').append('<span class="debug_message">Duplicate point at end removed.</span>');
+            //update xml?
+        }
+        this.guideOb.remove();
+        this.canvas.map.active = false;
+        this.canvas.map.currentObj = null;
+    },
+
+    progress:function() {
+        var newX = this.canvas.getMapX(this.canvas.cursor.snappedMouseX);
+        var newY = this.canvas.getMapY(this.canvas.cursor.snappedMouseY);
+
+        var prevPoint = this._points[this._points.length-1];
+
+        if(newX == prevPoint._x && newY == prevPoint._y) {
+            $('#debug_stream').append('<span class="debug_message">Duplicate point.</span>');
+            return;
+        }
+        this._points.push( new WallPoint(newX, newY) );
+        this.render();
+    },
+
+    guide:function() {
+        this.guideOb.remove();
+        var guideArr = []
+        guideArr = guideArr.concat(
+            [
+             'M',
+             this.canvas.getRealX(this._points[this._points.length-1]._x),
+             this.canvas.getRealY(this._points[this._points.length-1]._y)
+            ]
+        );
+        guideArr = guideArr.concat(
+            [
+             'L',
+             this.canvas.cursor.snappedMouseX,
+             this.canvas.cursor.snappedMouseY
+            ]
+        );
+        this.guideOb = this.paper.path(guideArr).attr({stroke: "#aaa"});
+    },
+
+    render:function() {
+        this.ob.remove();
+        this.pathArr = [];
+        for(var i = 0; i < this._points.length; i++) {
+            if(i == 0) {
+                this.pathArr = this.pathArr.concat(
+                    [
+                     'M',
+                     this.canvas.getRealX(this._points[0]._x),
+                     this.canvas.getRealY(this._points[0]._y)
+                    ]
+                );
+            } else {
+                this.pathArr = this.pathArr.concat(
+                    [
+                     'L',
+                     this.canvas.getRealX(this._points[i]._x),
+                     this.canvas.getRealY(this._points[i]._y)
+                    ]
+                );
+            }
+        } 
+        this.ob = this.paper.path(this.pathArr).attr({stroke: "#333"});
+        $('#debug_stream').append('<span class="debug_message">1' + this.pathArr + '</span>');
     }
 }
