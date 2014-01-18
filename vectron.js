745dd39062b9cd20d6321ed485bfa74c2194fe4d
TJohnW <tristan.whitcher@gmail.com>
2014-01-18 03:33:08 -0600

    sexified

--- vectron.js
+++ vectron.js
@@ -11,6 +11,7 @@ window.onload = function() {
 
 
     $("#canvas_container").mousemove(function(event) {
+        event.pageX -= 60;
         vectron.cursor.render(event.pageX, event.pageY, vectron.spacing);
     });
 
@@ -172,8 +173,10 @@ function Zone(vectron, x, y, radius, type) {
     this.paper = vectron.paper;
 
     this.render();
-    $('#debug_box').append('<span class="debug_message">'
+    $('#debug_stream').append('<span class="debug_message">'
         + 'Zone added at (' + this._x + ', ' + this._y + ')</span>');
+    var elem = document.getElementById('debug_stream');
+    elem.scrollTop = elem.scrollHeight;
 
 }  
 
@@ -187,100 +190,3 @@ Zone.prototype = {
             this._radius*this.canvas.spacing).attr("stroke","#FF0000");
     }
 }
-
-    
-    /*
-    instead of this ^^^^^
-    we have each object that needs rendering have a render function, then in the resize callback, we just tell grid to render itself
-    and store the rendered object (which can be .remove()'d) in the like, Grid.g object.
-    */
-    
-
-
-/*
-    var middleX = $("#canvas_container").width()/2;
-    var middleY = $("#canvas_container").height()/2;
-
-    var snappedMouseX = middleX;
-    var snappedMouseY = middleY;
-
-    var gridSpacing = 10;
-
-    var cursor = paper.path(
-        ["M", middleX - 5, middleY - 5,
-         'L', middleX - 1, middleY - 1,
-         'M', middleX + 1, middleY + 1,
-         'L', middleX + 5, middleY + 5]
-    );
-
-    var guide = paper.circle(snappedMouseX, snappedMouseY, 20).attr("stroke",
-        "#FF0000").attr("stroke-dasharray", "-");
-
-    g = grid_draw(paper, width, height, gridSpacing);
-    g.click(function(event) {
-        console.log(this);
-        console.log(event.x, event.y);
-    });
-    paper.path(gridArray).attr("stroke", "#d6d6ec");
-    var dz = paper.circle($("#canvas_container").width()/2, 
-        $("#canvas_container").height()/2, 20).attr("stroke", "#FF0000");
-
-    //dz.remove();
-    window.onresize = function() {
-        paper.clear();
-        
-        width = $("#canvas_container").width();
-        height = $("#canvas_container").height();
-
-        middleX = $("#canvas_container").width()/2;
-        middleY = $("#canvas_container").height()/2;
-
-        paper.setSize(width, height);
-
-        g = grid_draw(paper, width, height, gridSpacing);
-        g.click(function(event) {
-            console.log(this);
-            console.log(event.x, event.y);
-        });
-        //paper.circle($("#canvas_container").width()/2, 
-            //$("#canvas_container").height()/2, 20).attr("stroke", "#FF0000");
-    }
-
-    $("#canvas_container").on("click", function(e) {
-
-            e.preventDefault();
-            var x0 = e.pageX, y0 = e.pageY;
-            var x = x0, y = y0;
-            var elem = paper.getElementByPoint(x0, y0);
-            paper.circle(snappedMouseX, snappedMouseY, 20).attr("stroke",
-                "#FF0000");
-            console.log("WAWA");
-    });
-
-    $("#canvas_container").mousemove(function(event) {
-        
-        cursor.remove();
-        guide.remove();
-
-        snappedMouseX = middleX - Math.round((middleX - event.pageX) /
-            gridSpacing) * gridSpacing;
-        snappedMouseY = middleY - Math.round((middleY - event.pageY) /
-        gridSpacing) * gridSpacing;
-
-        cursor = paper.path(
-            ['M', snappedMouseX - 7, snappedMouseY,
-             'L', snappedMouseX - 2, snappedMouseY,
-             'M', snappedMouseX + 2, snappedMouseY,
-             'L', snappedMouseX + 7, snappedMouseY,
-             'M', snappedMouseX, snappedMouseY - 7,
-             'L', snappedMouseX, snappedMouseY - 2,
-             'M', snappedMouseX, snappedMouseY + 2,
-             'L', snappedMouseX, snappedMouseY + 7]
-        );
-        guide = paper.circle(snappedMouseX, snappedMouseY, 20).attr("stroke",
-            "#FF0000").attr("stroke-dasharray", "--..");
-    });
-}
-
-
-*/
