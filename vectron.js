475d4b5d8e9d75ed36a10346bae46525cea356a8
TJohnW <tristan.whitcher@gmail.com>
2014-01-18 00:29:23 -0600

    Working grid resize

--- vectron.js
+++ vectron.js
@@ -1,9 +1,70 @@
+//********//********//********//********//********//********//********//********
 window.onload = function() {
-	var width = $("#canvas_container").width();
-	var height = $("#canvas_container").height();
-    var paper = new Raphael(document.getElementById('canvas_container'),
-        width, height);
 
+    var vectron = new Canvas();
+    vectron.render( 10 );
+
+    var Aamap = new Aamap();
+    
+    window.onresize = function() {
+        vectron.render(vectron.spacing);
+    }
+    
+}
+
+
+function Canvas() {
+    this.width = $("#canvas_container").width();
+    this.height = $("#canvas_container").height();
+    this.paper = new Raphael(document.getElementById('canvas_container'), 
+        this.width, this.height);
+    this.spacing = 10;
+}  
+
+Canvas.prototype = {
+    constructor: Canvas,
+    
+    render:function( gridSpacing ) {
+        this.paper.clear();
+        this.width = $("#canvas_container").width();
+        this.height = $("#canvas_container").height();
+        this.paper.setSize(this.width, this.height);
+        this.spacing = gridSpacing;
+        this.draw_grid();
+    },
+    
+    draw_grid:function() {
+        var gridArray = [];
+        for(var i=this.width/2; i < this.width; i+= this.spacing) {
+            gridArray = gridArray.concat(["M", i, this.height, "L", i, 0]);
+        }
+    
+        for(var i=this.width/2; i > 0; i -= this.spacing) {
+            gridArray = gridArray.concat(["M", i,this. height, "L", i, 0]);
+        }
+    
+        for(var i=this.height/2; i < this.height; i+= this.spacing) {
+            gridArray = gridArray.concat(["M", this.width, i, "L", 0, i]);
+        }
+    
+        for(var i=this.height/2; i > 0; i -= this.spacing) {
+            gridArray = gridArray.concat(["M", this.width, i, "L", 0, i]);
+        }
+        
+        this.grid = this.paper.path(gridArray).attr("stroke", "#d6d6ec");
+    }
+}
+    
+    
+    /*
+    instead of this ^^^^^
+    we have each object that needs rendering have a render function, then in the resize callback, we just tell grid to render itself
+    and store the rendered object (which can be .remove()'d) in the like, Grid.g object.
+    */
+    
+
+
+/*
     var middleX = $("#canvas_container").width()/2;
     var middleY = $("#canvas_container").height()/2;
 
@@ -13,46 +74,46 @@ window.onload = function() {
     var gridSpacing = 10;
 
     var cursor = paper.path(
-    	["M", middleX - 5, middleY - 5,
-    	 'L', middleX - 1, middleY - 1,
-    	 'M', middleX + 1, middleY + 1,
-    	 'L', middleX + 5, middleY + 5]
+        ["M", middleX - 5, middleY - 5,
+         'L', middleX - 1, middleY - 1,
+         'M', middleX + 1, middleY + 1,
+         'L', middleX + 5, middleY + 5]
     );
 
     var guide = paper.circle(snappedMouseX, snappedMouseY, 20).attr("stroke",
         "#FF0000").attr("stroke-dasharray", "-");
 
     g = grid_draw(paper, width, height, gridSpacing);
-	g.click(function(event) {
-	    console.log(this);
-	    console.log(event.x, event.y);
-	});
+    g.click(function(event) {
+        console.log(this);
+        console.log(event.x, event.y);
+    });
     paper.path(gridArray).attr("stroke", "#d6d6ec");
     var dz = paper.circle($("#canvas_container").width()/2, 
         $("#canvas_container").height()/2, 20).attr("stroke", "#FF0000");
 
     //dz.remove();
     window.onresize = function() {
-	    paper.clear();
+        paper.clear();
         
-	    width = $("#canvas_container").width();
-		height = $("#canvas_container").height();
+        width = $("#canvas_container").width();
+        height = $("#canvas_container").height();
 
         middleX = $("#canvas_container").width()/2;
         middleY = $("#canvas_container").height()/2;
 
-		paper.setSize(width, height);
+        paper.setSize(width, height);
 
-		g = grid_draw(paper, width, height, gridSpacing);
-		g.click(function(event) {
-		    console.log(this);
-		    console.log(event.x, event.y);
-		});
-	    //paper.circle($("#canvas_container").width()/2, 
+        g = grid_draw(paper, width, height, gridSpacing);
+        g.click(function(event) {
+            console.log(this);
+            console.log(event.x, event.y);
+        });
+        //paper.circle($("#canvas_container").width()/2, 
             //$("#canvas_container").height()/2, 20).attr("stroke", "#FF0000");
-	}
+    }
 
-	$("#canvas_container").on("click", function(e) {
+    $("#canvas_container").on("click", function(e) {
 
             e.preventDefault();
             var x0 = e.pageX, y0 = e.pageY;
@@ -60,11 +121,11 @@ window.onload = function() {
             var elem = paper.getElementByPoint(x0, y0);
             paper.circle(snappedMouseX, snappedMouseY, 20).attr("stroke",
                 "#FF0000");
-     	    console.log("WAWA");
+            console.log("WAWA");
     });
 
     $("#canvas_container").mousemove(function(event) {
-		
+        
         cursor.remove();
         guide.remove();
 
@@ -83,38 +144,10 @@ window.onload = function() {
              'M', snappedMouseX, snappedMouseY + 2,
              'L', snappedMouseX, snappedMouseY + 7]
         );
-
         guide = paper.circle(snappedMouseX, snappedMouseY, 20).attr("stroke",
             "#FF0000").attr("stroke-dasharray", "--..");
     });
 }
 
-function grid_draw(canvas, width, height, gridSpacing) {
-	gridArray = [];
-	for(var i=width/2; i < width; i+= gridSpacing) {
-    	gridArray = gridArray.concat(["M", i, height, "L", i, 0]);
-        //paper.path(
-        	//["M", i, height, "L", i, 0]
-        //).attr("stroke", '#d6d6ec');
-    }
 
-    for(var i=width/2; i > 0; i -= gridSpacing) {
-        //paper.path(
-        gridArray = gridArray.concat(["M", i, height, "L", i, 0]);
-        //).attr("stroke", '#d6d6ec');
-    }
-
-    for(var i=height/2; i < height; i+= gridSpacing) {
-        //paper.path(
-        gridArray = gridArray.concat(["M", width, i, "L", 0, i]);
-        //).attr("stroke", '#d6d6ec');
-    }
-
-    for(var i=height/2; i > 0; i -= gridSpacing) {
-        //paper.path(
-        gridArray = gridArray.concat(["M", width, i, "L", 0, i]);
-        //).attr("stroke", '#d6d6ec');
-    }
-   	
-   	return canvas.path(gridArray).attr("stroke", "#d6d6ec");
-}
+*/
