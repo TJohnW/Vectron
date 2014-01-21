7ecf68807ea53760a2ee6ea9602a1a356acf4096
David Dubois <ddubois@jotunstudios.com>
2014-01-21 00:31:16 -0500

    Fixed tools again

--- js/vectron-0.1/_vectron/EventHandler.js
+++ js/vectron-0.1/_vectron/EventHandler.js
@@ -37,15 +37,10 @@ function EventHandler(vectron) {
         event.pageX -= 50;
         vectron.cursor.render(event.pageX, event.pageY, vectron.map.zoom);
 
-        if(vectron.map.currentTool instanceof WallTool) {
-        	if(vectron.map.wallTool.active)
-            	vectron.map.currentTool.currentObj.guide();
-
-        } else if(vectron.map.currentTool instanceof ZoneTool) {
+        if(vectron.map.currentTool instanceof ZoneTool) {
         	vectron.map.currentTool.guide();
-
-        } else if(vectron.map.currentTool instanceof SpawnTool) {
-            if(vectron.map.spawnTool.active)
+        } else {
+            if(vectron.map.currentTool.active)
                 vectron.map.currentTool.currentObj.guide();
         }
 
